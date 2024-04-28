c1111bd789a3fbfa6e0bc6055b95d732a3b275ed
andyjuang <andyjuang840502@gmail.com>
2024-04-28 11:30:17 +0800

    test

--- reservation.php
+++ reservation.php
@@ -95,8 +95,8 @@
         function test(){
             $.ajax({
                 url: "test.php",
-                dataType:"text",
-                // dataType:"json",
+                //dataType:"text",
+                dataType:"json",
                 success: function(result) {
                     console.log(result);
                 }
